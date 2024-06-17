9ba654a371bf086191a454d15acc1e2e88ba9caa
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-17 10:58:14 +0900

    10.5弾を発射できるようにする

--- syuuting/src/GameWorld.java
+++ syuuting/src/GameWorld.java
@@ -1,5 +1,6 @@
+import java.util.Vector;
 
 public class GameWorld {
 	static Player player;
-
+	static Vector<PlayerBullet> playerBullets;
 }
--- syuuting/src/Player.java
+++ syuuting/src/Player.java
@@ -1,8 +1,43 @@
 
-public class Player extends Character{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Player extends Character implements KeyListener{
 	public Player(double x, double y,double vx, double vy) {
 		//Characterクラスのコンストラクト呼び出し（8章）
 		super(x,y,vx,vy);
 	}
+	public void keyPressed(KeyEvent e) {
+		//キーボードが押された時の処理
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=-5;
+		}
+	}
+	public void keyReleased(KeyEvent e) {
+		//キーボードが離された時の処理（9章）
+		if  (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+		}
+	}
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	public void move() {
+		
+		super.move();
+		if (x<0) x=0;
+		if(x>370) x=370;
+			
+		}
+	}
 
-}
+
