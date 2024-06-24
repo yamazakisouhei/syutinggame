d20e158fd71cb7740811b1a833ff86c8be5a2833
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-24 10:18:54 +0900

    11,2敵を動かす

--- syuuting/src/GameFrame.java
+++ syuuting/src/GameFrame.java
@@ -5,26 +5,27 @@ public class GameFrame extends MyFrame{
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 		while (true) {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
-			int i=0;
-			while (i<GameWorld.playerBullets.size()) {
-				PlayerBullet b=GameWorld.playerBullets.get(i);
-				b.draw(this);
-				b.move();
-				if (b.y<0) {
-					GameWorld.playerBullets.remove(i);
-				}else {
-					i++;
-				}
+			movePlayerBullets();
+			moveEnemies();
 			}
 			sleep(0.03);
 		}
-		
-		
+	public void moveEnemies () {
+		for (int i=0 ; i<GameWorld.enemies.size();i++) {
+			Enemy e=GameWorld.enemies.get(i);
+			e.draw(this);
+			e.move();
+		}
 	}
+		
+		
+	
 	public void movePlayerBullets() {
 		int i=0;
 		while (i<GameWorld.playerBullets.size()) {
