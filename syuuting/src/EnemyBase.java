13238b2e549d6fd730c1fd1f53081d3c44ea260c
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-24 10:34:25 +0900

    11.3敵の動作を定義する

--- syuuting/src/EnemyBase.java
+++ syuuting/src/EnemyBase.java
@@ -1,4 +1,13 @@
 
-public class EnemyBase {
+public class EnemyBase extends Enemy{
+	public EnemyBase(double x,double y, double vx, double vy) {
+		super(x,y,vx,vy);
+		
+	}
+	public void move() {
+		super.move();
+		if (x>300) vx=-1;
+		if (x<100) vx=1;
+	}
 
 }
