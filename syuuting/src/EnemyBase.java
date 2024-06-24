87dd1fec4ef100e8c5a8b13a3d2d73ef378c2e9f
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-24 10:42:27 +0900

    11.4敵を出現させる

--- syuuting/src/EnemyBase.java
+++ syuuting/src/EnemyBase.java
@@ -2,12 +2,21 @@
 public class EnemyBase extends Enemy{
 	public EnemyBase(double x,double y, double vx, double vy) {
 		super(x,y,vx,vy);
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillOcal(x,y,32,32);
+		f.setColor(200,200,200);
+		f.fillOval(x-16, y+8, 64, 16);
+	}
 		
 	}
 	public void move() {
 		super.move();
 		if (x>300) vx=-1;
 		if (x<100) vx=1;
+		if (Math.random()<0.05) {
+			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
+		}
 	}
 
 }
