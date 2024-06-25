a25090be3ced3cb4aec5f05b7f14e68e0e5709dc
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-25 10:19:02 +0900

    12.4敵に耐久力をつける

--- syuuting/src/GameFrame.java
+++ syuuting/src/GameFrame.java
@@ -66,8 +66,11 @@ public class GameFrame extends MyFrame{
 				if(checkHit(e,b)) {
 					System.out.println("あたり");
 					hits++;
+					e.life--;
+				}
+				if(e.life<=0) {
 					GameWorld.enemies.remove(j);
-				} else {
+				}else {
 					j++;
 				}
 				}
