e431140af5e0896ad764b57cb361a069cefbe47d
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-25 10:43:44 +0900

    12.5敵の種類によって耐久力を変更する

--- syuuting/src/EnemyBase.java
+++ syuuting/src/EnemyBase.java
@@ -1,7 +1,10 @@
 
 public class EnemyBase extends Enemy{
+	
 	public EnemyBase(double x,double y, double vx, double vy) {
 		super(x,y,vx,vy);
+		life=20;
+	}
 		public void move() {
 			super.move();
 			if (x>300) vx=-1;
@@ -11,17 +14,17 @@ public class EnemyBase extends Enemy{
 			}
 			if (Math.random()<0.05) {
 				GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
-			}
+			}}
 		
 	public void draw(MyFrame f) {
 		f.setColor(0, 128, 0);
-		f.fillOcal(x,y,32,32);
+		f.fillOval(x,y,32,32);
 		f.setColor(200,200,200);
 		f.fillOval(x-16, y+8, 64, 16);
-	}}
-		
 	}
+		
 	
 	
-
 }
+
+
