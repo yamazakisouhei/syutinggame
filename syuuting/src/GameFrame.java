accb7e1ab39a866dd0c632e722a9b381324c6c05
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-25 10:59:19 +0900

    13.1すべての敵を倒したら「クリア」を表示する

--- syuuting/src/GameFrame.java
+++ syuuting/src/GameFrame.java
@@ -15,6 +15,10 @@ public class GameFrame extends MyFrame{
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+			if (GameWorld.enemies.size()==0) {//★A敵が全滅した？
+				setColor(0,0,0);
+				drawString("クリア！",100,200,40);
+			}
 			sleep(0.03);
 			}
 			
@@ -24,6 +28,15 @@ public class GameFrame extends MyFrame{
 			Enemy e=GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+		int i=0;
+		while (i<GameWorld.enemies.size()) {
+			Enemy e=GameWorld.enemies.get(i);
+			if (e.y>400) {
+				GameWorld.enemies.remove(i);
+			} else {
+				i++;
+			}
 		}
 	}
 		
