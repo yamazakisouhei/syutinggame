67822afeaa13253009e1740afacb16ce00b76261
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-06-25 10:12:59 +0900

    12.3衝突判定を共通化（メソッド化）

--- syuuting/src/GameFrame.java
+++ syuuting/src/GameFrame.java
@@ -13,7 +13,8 @@ public class GameFrame extends MyFrame{
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
-			 chelPlayerAndEnemies();
+			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnemies();
 			sleep(0.03);
 			}
 			
@@ -43,33 +44,51 @@ public class GameFrame extends MyFrame{
 			
 		}
 	}
-	public void chelPlayerAndEnemies() {
-		for (int i=0 ; i<GameWorld.enemies.size(); i+;) {
+	public void checkPlayerAndEnemies() {
+		for (int i=0; i<GameWorld.enemies.size(); i++) {
 			Enemy e=GameWorld.enemies.get(i);
-			if (Math.abs(e.x-GameWorld.player.x)<=30 &&
-				Math.abs(e.y-GameWorld.player.y)<=30) {
-				System.out.printlb("やられた！");
+			if (checkHit(GameWorld.player,e)) {
+				System.out.println("やられた！");
 				GameWorld.player.y=-1000;
 			}
+			}
 		}
-	}
-	public void checlPlayerBulletsSndEnemies() {
+	
+	public void checkPlayerBulletsAndEnemies() {
 		int i=0;
 		while (i<GameWorld.playerBullets.size()) {
 			//プレイヤー弾一つ一つについて、変数ｂに入れて繰り返し実行する
 			PlayerBullet b=GameWorld.playerBullets.get(i);
 			int j=0;
+			int hits=0;
 			while (j<GameWorld.enemies.size()) {
 				Enemy e=GameWorld.enemies.get(j);
-				if(Math.abs(e.x-b.x)<=30 &&
-					Math.abs(e.y-b.y)<=30) {
+				if(checkHit(e,b)) {
 					System.out.println("あたり");
+					hits++;
+					GameWorld.enemies.remove(j);
+				} else {
+					j++;
 				}
-				j++;	
-				
-			}
-			j++;
+				}
+		
+		
+		if (hits>0) {
+			GameWorld.playerBullets.remove(i);
+		} else {
+			i++;
 		}
 	}
 
+	}
+	public boolean checkHit(Character a,Character b) { //★A
+		if (Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30) { //★B
+			return true;  //★C
+			
+		} else {
+			return false;  //★D
+		}
+		
+	
+	}
 }
